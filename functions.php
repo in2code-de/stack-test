44ebe6ffc92c8ac9f2c9228097577e6b7dbf9532
Oliver Eglseder <oliver.eglseder@co-stack.com>
2024-05-29 11:53:53 +0200

    [BUGFIX] Allow resolving string values and to string values

--- functions.php
+++ functions.php
@@ -9,6 +9,8 @@ use Facebook\WebDriver\WebDriver;
 use Facebook\WebDriver\WebDriverBy;
 use Facebook\WebDriver\WebDriverElement;
 
+use function is_string;
+
 function resolveElementText(WebDriverElement $element): string
 {
     return match ($element->getTagName()) {
@@ -20,13 +22,16 @@ function resolveElementText(WebDriverElement $element): string
 
 /**
  * @param WebDriver $driver
- * @param WebDriverBy|array<WebDriverBy> $by
+ * @param WebDriverBy|array<WebDriverBy>|string $by
  * @return WebDriverElement|array<WebDriverElement>
  */
-function resolve(WebDriver $driver, WebDriverBy|array $by): WebDriverElement|array
+function resolve(WebDriver $driver, WebDriverBy|array|string $by): WebDriverElement|array|string
 {
     if ($by instanceof WebDriverBy) {
         return $driver->findElement($by);
+    }
+    if (is_string($by)) {
+        return $by;
     }
     foreach ($by as $index => $bye) {
         $by[$index] = resolve($driver, $bye);
